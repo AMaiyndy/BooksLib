fba887dba89daaa575feef3643d819a27978c81a
Arat Mayyndy <Arat Mayyndy>
2017-02-20 22:30:30 +0300

    Modifying Book class.

--- src/main/java/com/may/bookslib/model/Book.java
+++ src/main/java/com/may/bookslib/model/Book.java
@@ -1,4 +1,49 @@
 package com.may.bookslib.model;
 
 public class Book {
+    private long id;
+    private String bookTitle;
+    private String bookAuthor;
+
+    public Book() {
+    }
+
+    public Book(long id, String bookTitle, String bookAuthor) {
+        this.id = id;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                '}';
+    }
 }
